4dc7074b148918869797a3a52b4619ed6ef312c7
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-02 15:18:09 +0300

    ChatBot 2 stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,3 +1,3 @@
-print("""Hello! My name is NedoBot.
-I was created in 2021.""")
-
+print("Hello! My name is NedoBot.\nI was created in 2021.")
+name = input("Please, remind me your name.\n>")
+print("What a great name you have," + name + "!")
