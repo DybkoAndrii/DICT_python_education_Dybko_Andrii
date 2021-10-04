eb9257f1657fa25f6b53ce3c2863a4b70508e8a8
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-04 08:48:13 +0300

    Hangman 3 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,10 @@
+import random
+
 print("HANGMAN")
+qwe1 = ["python", "java", "javascript", "php"]
+qwe2 = random.choice(qwe1)
 qwe0 = input("Guess the word:")
-if qwe0 == "python":
+if qwe0 == qwe2:
     print("You survived!")
 else:
     print("You lost!")
