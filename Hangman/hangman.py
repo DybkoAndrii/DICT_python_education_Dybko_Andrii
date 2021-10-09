53e8162529b4e1052fda7338096f36873be9cae0
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-09 13:42:21 +0300

    Hangman 4 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,7 +3,8 @@ import random
 print("HANGMAN")
 qwe1 = ["python", "java", "javascript", "php"]
 qwe2 = random.choice(qwe1)
-qwe0 = input("Guess the word:")
+qwe3 = {'python':"pyt___", 'java':"jav_", 'javascript':"jav_______", 'php':"php"}
+qwe0 = input("Guess the word " + qwe3[qwe2] + ":")
 if qwe0 == qwe2:
     print("You survived!")
 else:
