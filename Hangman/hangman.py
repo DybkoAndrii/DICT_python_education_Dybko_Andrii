861ac19aee8f350dcb7357d42124dd25bbec26a6
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-10 12:23:17 +0300

    Hangman 7 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,6 +1,5 @@
 import random
 
-print("HANGMAN")
 words = ["python", "java", "javascript", "php"]
 word = random.choice(words)
 word_set = set(word)
@@ -21,15 +20,28 @@ def playing():
     while life > 0:
         print('')
         letter1 = input("Input a letter: ")
+        if len(letter1) == 0 or len(letter1) > 1:
+            print('You should input a single letter')
+            print('')
+            de_sh()
+            continue
+        if letter1.capitalize() == letter1:
+            print('Please enter a lowercase English letter')
+            print('')
+            de_sh()
+            continue
         if letter1 in letter:
-            print("No improvements")
-            life -= 1
+            print("You've already guessed this letter.")
+        if letter1 in wrong_letter:
+            print("You've already guessed this letter.")
         for qwe1 in word:
             if letter1 == qwe1:
                 letter.append(letter1)
         if letter1 not in word:
-            print("That letter doesn't appear in the word")
-            life -= 1
+            if letter1 not in wrong_letter:
+                wrong_letter.append(letter1)
+                print("That letter doesn't appear in the word")
+                life -= 1
         print('')
         if life > 0:
             de_sh()
