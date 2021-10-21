ef073aa21fdc18602221c52a723048cd2e62c881
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-21 18:40:59 +0300

    TicTacToe 2 stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,3 +1,15 @@
-print("X O X")
-print("O X O")
-print("X O X")
+cells = list(input("Enter cells: "))
+print("---------")
+print("| ", end="")
+for u in cells[:3]:
+    print(u, end=" ")
+print("|")
+print("| ", end="")
+for o in cells[3:6]:
+    print(o, end=" ")
+print("|")
+print("| ", end="")
+for i in cells[6:]:
+    print(i, end=" ")
+print("|")
+print("---------")
