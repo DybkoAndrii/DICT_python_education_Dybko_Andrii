79ec9fd9826711b9d796686f624fa4956e9bd070
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-10-31 21:08:22 +0200

    CreditCalculator 3 stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,31 +1,45 @@
-principal = int(input("Enter the loan principal: "))
+import math
 
 
-def pay_men_t():
-    month = int(input("Enter the number of months: "))
-    qwe = principal / month
-    payment = qwe.__ceil__()
-    last_payment = principal - (month - 1) * payment
-    if last_payment == payment:
-        print("")
-        print("Your monthly payment = " + str(payment))
+def n_of_mp():
+    p = int(input("Enter the loan principal: "))
+    a = int(input("Enter the monthly payment: "))
+    ni = int(input("Enter the loan interest: "))
+    i = ni / 1200
+    n = math.log(a / (a - i * p), 1 + i)
+    n_n2 = n.__ceil__() % 12
+    n_n1 = int((n.__ceil__() - n_n2) / 12)
+    if n_n2 == 0:
+        print("It will take " + str(n_n1) + " years")
     else:
-        print("")
-        print("Your monthly payment = " + str(payment) + " and the last payment = " + str(last_payment) + ".")
+        print("It will take " + str(n_n1) + " years and " + str(n_n2) + " months to repay this loan!")
 
 
-def n_payments():
-    nam = int(input("Enter the monthly payment: "))
-    qwe1 = principal / nam
-    months = qwe1.__ceil__()
-    print("")
-    print("It will take " + str(months) + " months to repay the loan")
+def am_pa():
+    p = int(input("Enter the loan principal: "))
+    n_n = int(input("Enter the number of periods: "))
+    ni = int(input("Enter the loan interest: "))
+    i = ni / 1200
+    a = (p * i * (1 + i) ** n_n) / (((i + 1) ** n_n) - 1)
+    print("Your monthly payment = " + str(a.__ceil__()) + " !")
+
+
+def for_lnp():
+    a = float(input("Enter the annuity payment: "))
+    n_n = float(input("Enter the number of periods: "))
+    ni = float(input("Enter the loan interest: "))
+    i = ni / 1200
+    p = a / ((i * (1 + i) ** n_n) / (((i + 1) ** n_n) - 1))
+    print("Your loan principal = " + str(int(p)) + " !")
 
 
 action = input("""What do you want to calculate?
-type "m" – for number of monthly payments,
-type "p" – for the monthly payment: """)
-if action == "p":
-    pay_men_t()
-elif action == "m":
-    n_payments()
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal: """)
+if action == "n":
+    n_of_mp()
+elif action == "a":
+    am_pa()
+elif action == "p":
+    for_lnp()
