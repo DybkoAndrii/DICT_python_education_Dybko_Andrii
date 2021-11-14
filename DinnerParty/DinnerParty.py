a7082c6d37a9ae1d6a163d74ff6dcf9c503227ba
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-11-14 23:18:49 +0200

    DinnerParty 3 stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,9 +1,16 @@
+import random
+
 numb = int(input("Enter the number of friends joining (including you): "))
 if numb > 0:
     print("Enter the name of every friend (including you), each on a new line:")
     friends = [input() for i in range(numb)]
     amount = int(input("Enter the total amount: "))
     value_dict = {i: round(amount / numb, 2) for i in friends}
-    print(value_dict)
+    action = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ')
+    if action == "Yes" or action == "yes":
+        lucky = random.choice(friends)
+        print("{} is the lucky one!".format(lucky))
+    else:
+        print("No one is going to be lucky")
 else:
     print("No one is joining for the party")
