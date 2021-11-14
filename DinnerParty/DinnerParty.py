da49964d0c6ee1b1311bb965cf4415611dfb220d
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-11-15 00:11:53 +0200

    DinnerParty 4 stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -8,9 +8,14 @@ if numb > 0:
     value_dict = {i: round(amount / numb, 2) for i in friends}
     action = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ')
     if action == "Yes" or action == "yes":
-        lucky = random.choice(friends)
-        print("{} is the lucky one!".format(lucky))
+        lucky = random.choice(range(len(friends)))
+        print("{} is the lucky one!".format(friends[lucky]))
+        value_dict_1 = {i: round(amount / (numb - 1), 2) for i in friends}
+        value_dict_l = list(value_dict_1.keys())
+        value_dict_1[value_dict_l[lucky]] = 0
+        print(value_dict_1)
     else:
         print("No one is going to be lucky")
+        print(value_dict)
 else:
     print("No one is joining for the party")
