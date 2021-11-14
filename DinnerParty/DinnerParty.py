95e1217899fb0659cbbd661d83d0ee22f35e5c18
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-11-14 22:39:49 +0200

    DinnerParty 2 stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -2,5 +2,8 @@ numb = int(input("Enter the number of friends joining (including you): "))
 if numb > 0:
     print("Enter the name of every friend (including you), each on a new line:")
     friends = [input() for i in range(numb)]
-    value_dict = {i: 0 for i in friends}
+    amount = int(input("Enter the total amount: "))
+    value_dict = {i: round(amount / numb, 2) for i in friends}
     print(value_dict)
+else:
+    print("No one is joining for the party")
