84d2482d31d9924b907002315297c1fac89748fd
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-11-21 20:01:45 +0200

    dominoes 3 stage

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -16,14 +16,83 @@ for i in lst:
         snake = i
 if snake in player:
     player.remove(snake)
-    status = "Computer is about to make a move. Press Enter to continue..."
+    status = "computer"
 else:
     computer.remove(snake)
-    status = "It's your turn to make a move. Enter your command"
-print("=" * 70)
-print("Stock size: {}".format(len(dmn)))
-print("Computer pieces: {}".format(len(computer)))
-print("\n{}\n".format(snake))
-for i in range(len(player)):
-    print("{}:{}".format(i+1, player[i]))
-print("\nStatus: {}.".format(status))
+    status = "player"
+
+
+def playing_field():
+    print("=" * 70)
+    print("Stock size: {}".format(len(dmn)))
+    print("Computer pieces: {}".format(len(computer)))
+    if len(table) > 6:
+        print("\n{}{}{}\n".format(table[:3], "...", table[-3:]))
+    else:
+        print("\n{}\n".format(table))
+    for i in range(len(player)):
+        print("{}:{}".format(i + 1, player[i]))
+
+
+def comp_turn():
+    z = random.choice(range(-len(computer), len(computer)+1))
+    if z > 0:
+        table.append(computer[z-1])
+        computer.remove(computer[z-1])
+    elif z < 0:
+        table.insert(0, computer[-z-1])
+        computer.remove(computer[-z-1])
+    else:
+        table.append(dmn[0])
+        dmn.pop(0)
+
+
+def pl_turn(qwe1):
+    if qwe1 > 0:
+        table.append(player[qwe1-1])
+        player.remove(player[qwe1-1])
+    elif qwe1 < 0:
+        table.insert(0, player[-qwe1-1])
+        player.remove(player[-qwe1-1])
+    else:
+        table.append(dmn[0])
+        dmn.pop(0)
+
+
+table = [snake]
+playing_field()
+while True:
+    if status == "computer":
+        print(input("Status: Computer is about to make a move. Press Enter to continue...\n"))
+        comp_turn()
+        status = "player"
+    else:
+        pl_movie = input("Status: It's your turn to make a move. Enter your command.\n ")
+        try:
+            int(pl_movie)
+        except ValueError:
+            print("Invalid input. Please try again.")
+            continue
+        else:
+            if int(pl_movie) in range(-len(player), len(player)+1):
+                pl_turn(int(pl_movie))
+                status = "computer"
+            else:
+                print("Invalid input. Please try again.")
+                continue
+    playing_field()
+    if len(computer) == 0:
+        print("Status: The game is over. The computer won!")
+        break
+    elif len(player) == 0:
+        print("Status: The game is over. You won!")
+        break
+    elif table[0][0] == table[-1][-1]:
+        score = 0
+        for t in range(len(table)):
+            for s in table[t]:
+                if s == table[0][0]:
+                    score += 1
+        if score == 8:
+            print("Status: The game is over. It's a draw!")
+            break
