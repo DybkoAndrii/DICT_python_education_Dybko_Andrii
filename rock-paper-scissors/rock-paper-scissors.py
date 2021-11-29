c225c1dc3c7a55faed9bce5375bdb6090055c4ad
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-11-29 21:07:38 +0200

    rock-paper-scissors 2 stage

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -1,4 +1,14 @@
-options = {"scissors": "rock", "paper": "scissors", "rock": "paper"}
+import random
+
+options = {"rock": "paper", "scissors": "rock", "paper": "scissors"}
+comp = random.choice(list(options.values()))
 option = input()
 if option in options:
-    print("Sorry, but the computer chose {}".format(options[option]))
+    if option == options[comp]:
+        print("Well done. The computer chose {} and failed".format(comp))
+    elif comp == options[option]:
+        print("Sorry, but the computer chose {}".format(comp))
+    else:
+        print("There is a draw {}".format(option))
+else:
+    print("Invalid input")
