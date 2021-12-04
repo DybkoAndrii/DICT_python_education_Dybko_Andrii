4fff1e876c9526765734f2b70de0a58137fad2ca
DybkoAndrii <a.i.dybko@student.khai.edu>
2021-12-04 13:50:37 +0200

    rock-paper-scissors 5 stage

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -1,23 +1,48 @@
 import random
 
+
+def take_options():
+    while True:
+        fun_pl_options = input().split(",")
+        fun_loses_opt = []
+        if len(fun_pl_options) == 0:
+            return {"rock": ["paper"], "scissors": ["rock"], "paper": ["scissors"]}
+        elif len(fun_pl_options) < 3:
+            print("you can enter at least 3 items")
+            continue
+        elif len(fun_pl_options) % 2 == 0:
+            print("Please enter an odd number of items")
+            continue
+        else:
+            for i in range(len(fun_pl_options)):
+                qwe = []
+                for j in range(int(len(fun_pl_options) / 2)):
+                    qwe.append(fun_pl_options[i - 1 - j])
+                fun_loses_opt.append(qwe)
+            return {fun_pl_options[i]: fun_loses_opt[i] for i in range(len(fun_pl_options))}
+
+
 with open("rating.txt", "r", encoding='utf-8') as f:
     text = f.read()
     file = text.split()
+
 if __name__ == "__main__":
-    options = {"rock": "paper", "scissors": "rock", "paper": "scissors"}
     player = input("Enter your name: ")
+    print("Hello, {}".format(player))
+    options = take_options()
+    print("Okay, let's start")
     if player in file:
         rating = int(file[file.index(player) + 1])
     else:
         rating = 0
     while True:
-        comp = random.choice(list(options.values()))
+        comp = random.choice(list(options.keys()))
         option = input()
         if option in options:
-            if option == options[comp]:
+            if option in options[comp]:
                 print("Well done. The computer chose {} and failed".format(comp))
                 rating += 100
-            elif comp == options[option]:
+            elif comp in options[option]:
                 print("Sorry, but the computer chose {}".format(comp))
             else:
                 print("There is a draw {}".format(option))
