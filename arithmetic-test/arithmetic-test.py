7f95e57a219cf383329d06c2a5c53f763dcec520
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-02-09 22:28:23 +0200

    arithmetic-test 2 stage

--- arithmetic-test/arithmetic-test.py
+++ arithmetic-test/arithmetic-test.py
@@ -1,6 +1,7 @@
 from random import randint
 
-if __name__ == "__main__":
+
+def st_1():
     action = randint(0, 2)
     num_1 = randint(2, 9)
     num_2 = randint(2, 9)
@@ -11,13 +12,24 @@ if __name__ == "__main__":
         answer = num_1 - num_2
     else:
         answer = num_1 * num_2
-    take = input(f"{num_1} {action_st[action]} {num_2}\n")
-    try:
-        print(answer)
-        if int(take) == answer:
-            print("Right!")
-        else:
-            print("Wrong!")
-    except ValueError:
-        print("Incorrect format")
+    print(f"{num_1} {action_st[action]} {num_2}")
+    return answer
 
+
+if __name__ == "__main__":
+    grade = 0
+    for i in range(5):
+        res = st_1()
+        while True:
+            take = input()
+            try:
+                if int(take) == res:
+                    print("Right!")
+                    grade += 1
+                else:
+                    print("Wrong!")
+            except ValueError:
+                print("Incorrect format")
+            else:
+                break
+    print(f"Your mark is {grade}/5.")
