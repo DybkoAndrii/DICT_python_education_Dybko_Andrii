f5228ca188922e3146dbfd10034c4a3e0a8b2f98
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-02-10 12:20:20 +0200

    arithmetic-test protection

--- arithmetic-test/arithmetic-test.py
+++ arithmetic-test/arithmetic-test.py
@@ -22,6 +22,35 @@ def st_2():
     return num ** 2
 
 
+def st_3():
+    action_1 = randint(0, 2)
+    action_2 = randint(0, 2)
+    num_1 = randint(2, 9)
+    num_2 = randint(2, 9)
+    num_3 = randint(2, 9)
+    action_st = ['+', '-', '*']
+    if action_1 == 0 and action_2 == 0:
+        answer = num_1 + num_2 + num_3
+    elif action_1 == 1 and action_2 == 0:
+        answer = num_1 - num_2 + num_3
+    elif action_1 == 2 and action_2 == 0:
+        answer = num_1 * num_2 + num_3
+    elif action_1 == 0 and action_2 == 1:
+        answer = num_1 + num_2 - num_3
+    elif action_1 == 1 and action_2 == 1:
+        answer = num_1 - num_2 - num_3
+    elif action_1 == 2 and action_2 == 1:
+        answer = num_1 * num_2 - num_3
+    elif action_1 == 0 and action_2 == 2:
+        answer = num_1 + num_2 - num_3
+    elif action_1 == 1 and action_2 == 2:
+        answer = num_1 - num_2 - num_3
+    else:
+        answer = num_1 * num_2 * num_3
+    print(f"{num_1} {action_st[action_1]} {num_2} {action_st[action_2]} {num_3}")
+    return answer
+
+
 def save(x, y, z):
     try:
         with open("results.txt", "a", encoding='utf-8') as f:
@@ -35,7 +64,8 @@ if __name__ == "__main__":
     while True:
         action = input("""Which level do you want? Enter a number:
 1 - simple operations with numbers 2-9
-2 - integral squares of 11-29\n""")
+2 - integral squares of 11-29
+3 - operations on three numbers 2-9\n""")
         grade = 0
         if action == "1":
             lvl = "1 (simple operations with numbers 2-9)"
@@ -59,6 +89,24 @@ if __name__ == "__main__":
             lvl = "2 (integral squares of 11-29)"
             for i in range(5):
                 res = st_2()
+                while True:
+                    take = input()
+                    try:
+                        if int(take) == res:
+                            print("Right!")
+                            grade += 1
+                        else:
+                            print("Wrong!")
+                    except ValueError:
+                        print("Incorrect format")
+                    else:
+                        break
+            print(f"Your mark is {grade}/5.")
+            break
+        elif action == "3":
+            lvl = "3 (operations on three numbers 2-9)"
+            for i in range(5):
+                res = st_3()
                 while True:
                     take = input()
                     try:
