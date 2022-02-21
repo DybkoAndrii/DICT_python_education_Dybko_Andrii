42c1abf194561014be6b747de4c0fe6b46cb5ba8
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-02-21 23:51:59 +0200

    currency-exchange 4 stage

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,18 +1,39 @@
 import requests
 
 
+def check_req(y, x):
+    try:
+        res = requests.get(f'http://www.floatrates.com/daily/{y}.json')
+        qwe = res.json()
+    except ValueError:
+        print("ERROR")
+        return "ERROR"
+    else:
+        return qwe[x]['rate']
+
+
 def main():
+    a = input("Enter your currency code: ")
+    cache = {'USD': check_req(a, 'usd'), 'EUR': check_req(a, 'eur')}
     while True:
-        try:
-            y = input("Please enter currency code: ")
-            res = requests.get(f'http://www.floatrates.com/daily/{y}.json')
-            qwe = res.json()
-        except ValueError:
-            print("ERROR")
-        else:
+        b = input("Enter the code of the required currency: ")
+        if b.lower() == "!exit":
             break
-    print(f"""USD: {qwe['usd']['rate']}
-EUR: {qwe['eur']['rate']}""")
+        while True:
+            try:
+                money = float(input("Enter the amount of money: "))
+            except ValueError:
+                print("ERROR")
+            else:
+                break
+        print("Checking the cache...")
+        if b.upper() in cache:
+            print("It is in the cache!")
+            print(f"You received {round(money * float(cache[b.upper()]), 2)} {b.upper()}")
+        else:
+            print("Sorry, but it is not in the cache!")
+            cache[b.upper()] = check_req(a, b.lower())
+            print(f"You received {round(money * float(cache[b.upper()]), 2)} {b.upper()}")
 
 
 if __name__ == "__main__":
