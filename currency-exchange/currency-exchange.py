528a37534cd32ea582ae2af98e3cd307354a21cd
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-02-21 21:55:08 +0200

    currency-exchange 3 stage

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,15 +1,18 @@
+import requests
+
+
 def main():
     while True:
         try:
-            y = float(input("Please, enter the number of my_coins you have: "))
+            y = input("Please enter currency code: ")
+            res = requests.get(f'http://www.floatrates.com/daily/{y}.json')
+            qwe = res.json()
         except ValueError:
             print("ERROR")
         else:
             break
-    print(f"""I will get {round(y * 0.82, 2)} ARS from the sale of {y} my_coins.
-I will get {round(y * 0.17, 2)} HNL from the sale of {y} my_coins.
-I will get {round(y * 1.9622, 2)} AUD from the sale of {y} my_coins.
-I will get {round(y * 0.208, 2)} MAD from the sale of {y} my_coins.""")
+    print(f"""USD: {qwe['usd']['rate']}
+EUR: {qwe['eur']['rate']}""")
 
 
 if __name__ == "__main__":
