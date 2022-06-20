a924bf9c5546e5fbf96bcde87a0a3b2a3220b8d9
DybkoAndrii <a.i.dybko@student.khai.edu>
2022-06-20 16:31:39 +0300

    web_page_scraper 2 stage

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,13 +1,16 @@
 import requests
+from bs4 import BeautifulSoup as Bs
 
 
 def main():
     url = input("Input the URL: ")
-    res = requests.get(url)
-    if 300 > int(res.status_code) >= 200:
-        quote = res.json()
-        if 'content' in quote:
-            print(quote['content'])
+    if 'title' in url.split('/') and 'imdb' in url.split('.'):
+        res = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        if int(res.status_code) == 200:
+            quote = Bs(res.text, 'html.parser')
+            print(quote.find('title'))
+            print('='*70)
+            print(quote.find('meta', {'name': 'description'}, ['content']))
         else:
             print("Invalid quote resource!")
     else:
